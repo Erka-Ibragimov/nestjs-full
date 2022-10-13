f6a2fae6910bc336c53cc20be2ac588540cd10fc
Erka-Ibragimov <erkaibragimov@gmail.com>
2022-10-13 14:45:01 +0500

    first commit

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate{
+    
+    constructor(private jwtService: JwtService){}
+    
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const req = context.switchToHttp().getRequest();
+        try{
+            const authHeader = req.headers.authorization;
+            const arr = authHeader.split(' ');
+            const bearer = arr[0];
+            const token = arr[1];
+            if(bearer !== 'Bearer' || !token){
+                throw new UnauthorizedException({message:'Пользователь не авторизован'})
+            }
+            const user = this.jwtService.verify(token);
+            req.user = user;
+            return true;
+        } catch(e){
+            console.log(e);
+            throw new UnauthorizedException({message:'Пользователь не авторизован'})
+        }
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "./pipes/validation.pipe";
+
+async function start() {
+   const PORT = process.env.PORT || 5000; 
+   const app = await NestFactory.create(AppModule);
+   const config  = new DocumentBuilder().setTitle('Erkin').setDescription('Ibragimov').setVersion('1.0.0').addTag("22.05.1999").build();
+   const document = SwaggerModule.createDocument(app,config);
+   SwaggerModule.setup('/api/docs',app,document);
+   app.useGlobalPipes( new ValidationPipe);
+   await app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`);
+   })
+}
+start();
